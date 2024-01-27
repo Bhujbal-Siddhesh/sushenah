dbf53d00ceaf5bc65d54f2a22ad68c4fc5eed95d
Bhujbal-Siddhesh <102919424+Bhujbal-Siddhesh@users.noreply.github.com>
2024-01-27 23:03:20 +0530

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -6,18 +6,18 @@ let aLoadedUnBlurImages = [];
 
 (function () {
     const aBlurImageLinks = [
-        "testImages/1.webp",
-        "testImages/2.webp",
-        "testImages/3.webp",
+        "https://bhujbal-siddhesh.github.io/sushenah/testImages/1.webp",
+        "https://bhujbal-siddhesh.github.io/sushenah/testImages/2.webp",
+        "https://bhujbal-siddhesh.github.io/sushenah/testImages/3.webp"
     ];
     const aUnBlurImageLinks = [
-        "testImages/01.webp",
-        "testImages/02.webp",
-        "testImages/03.webp",
+        "https://bhujbal-siddhesh.github.io/sushenah/testImages/01.webp",
+        "https://bhujbal-siddhesh.github.io/sushenah/testImages/02.webp",
+        "https://bhujbal-siddhesh.github.io/sushenah/testImages/03.webp"
     ];
     let aPairs = [];
     const aDivIds = ["Blur", "UnBlur"];
-    
+
     for (let nCount = 0; nCount < aUnBlurImageLinks.length; nCount++) {
         let nCountTemp = nCount;
         aPairs.push("Pair_" + ++nCountTemp);
@@ -29,6 +29,9 @@ let aLoadedUnBlurImages = [];
         //  creating parent div elements
         const oParentDivElement = document.createElement("div");
         oParentDivElement.setAttribute("id", aPairs[nCount_0]);
+        oParentDivElement.classList.add("d-flex");
+        // oParentDivElement.classList.add("flex-row");
+        oParentDivElement.classList.add("justify-content-center");
         oGalleryContainer.appendChild(oParentDivElement);
 
         const oParentDiv = {};
@@ -37,20 +40,13 @@ let aLoadedUnBlurImages = [];
 
         //  creating div elements for blur & unblur image elements
         for (let nCount_1 = 0; nCount_1 < aDivIds.length; nCount_1++) {
-            const oChildDivElement = document.createElement("div");
-            oChildDivElement.setAttribute("id", aPairs[nCount_0] + "_" + aDivIds[nCount_1]);
-            oParentDivElement.appendChild(oChildDivElement);
-
-            const oChildDiv = {};
-            oChildDiv.childDivID = aPairs[nCount_0] + "_" + aDivIds[nCount_1];
-            oChildDiv.childDivElement = oChildDivElement;
 
             //  creating image elements
             const oImageElement = document.createElement("img");
             oImageElement.setAttribute("id", aPairs[nCount_0] + "_" + aDivIds[nCount_1] + "_Img");
             oImageElement.setAttribute("alt", "");
-            oImageElement.setAttribute("width", "50%");
-            oImageElement.setAttribute("height", "50%");
+            oImageElement.setAttribute("width", "100%");
+            oImageElement.setAttribute("height", "auto");
 
             const oImageTag = {};
             oImageTag.imageTagID = aPairs[nCount_0] + "_" + aDivIds[nCount_1] + "_Img";
@@ -59,45 +55,35 @@ let aLoadedUnBlurImages = [];
                 oImageElement.setAttribute("src", aBlurImageLinks[nCount_0]);
                 oImageElement.setAttribute("onload", "loadUnBlurImages(" + oImageTag.imageTagID + ")");
                 oImageTag.imageTagElement = oImageElement;
-            }
-            else if (aDivIds[nCount_1] == "UnBlur") {
-                oChildDivElement.style.display = "none";
+            } else if (aDivIds[nCount_1] == "UnBlur") {
+                oImageElement.style.display = "none";
                 oImageElement.setAttribute("data-src", aUnBlurImageLinks[nCount_0]);
+                oImageElement.setAttribute("onload", "changeStyling(" + oImageTag.imageTagID + ")");
                 oImageTag.imageTagElement = oImageElement;
             }
 
-            oChildDiv.oImageElement = oImageTag;
-            oParentDiv["o" + aDivIds[nCount_1] + "DivElement"] = oChildDiv;//oBlurDivElement, oUnBlurDivElement
-            oChildDivElement.appendChild(oImageElement);
+            oParentDiv["o" + aDivIds[nCount_1] + "ImgElement"] = oImageTag;
+            oParentDivElement.appendChild(oImageElement);
         }
         aObjects.push(oParentDiv);
     }
-})()
+})();
 
 function loadUnBlurImages(oImageElement) {
     aLoadedBlurImages.push(oImageElement.id);
+    if (aLoadedBlurImages.length === aObjects.length)
+        for (let nCount = 0; nCount < aLoadedBlurImages.length; nCount++)
+            if (aObjects[nCount].oUnBlurImgElement.imageTagElement.hasAttribute("src") == false) {
+                aObjects[nCount].oUnBlurImgElement.imageTagElement.setAttribute("src", aObjects[nCount].oUnBlurImgElement.imageTagElement.getAttribute("data-src"));
+            }
 }
 
-let aIntervalLoadedImages = [];
-let myInterval = setInterval(() => {
-    if (aIntervalLoadedImages.length == aObjects.length)
-        clearInterval(myInterval);
-
-    for (let nCount = 0; nCount < aLoadedBlurImages.length; nCount++) {
-        if (aIntervalLoadedImages.includes(aObjects[nCount].oUnBlurDivElement.oImageElement.imageTagID))
-            continue;
-
-        if (aObjects[nCount].oUnBlurDivElement.oImageElement.imageTagElement.hasAttribute("src") == false) {
-            aObjects[nCount].oUnBlurDivElement.oImageElement.imageTagElement.setAttribute("src", aObjects[nCount].oUnBlurDivElement.oImageElement.imageTagElement.getAttribute("data-src"));
+function changeStyling(oImageElement) {
+    for (let nCount = 0; nCount < aObjects.length; nCount++)
+        if (!aLoadedUnBlurImages.includes(oImageElement.id) && aObjects[nCount].oUnBlurImgElement.imageTagElement.id === oImageElement.id) {
+            aObjects[nCount].oBlurImgElement.imageTagElement.style.display = "none";
+            aObjects[nCount].oUnBlurImgElement.imageTagElement.style.display = "inline";
+            aLoadedUnBlurImages.push(oImageElement.id);
             break;
         }
-
-        if (aObjects[nCount].oUnBlurDivElement.oImageElement.imageTagElement.complete == false)
-            break;
-        else {
-            aObjects[nCount].oBlurDivElement.childDivElement.style.display = "none";
-            aObjects[nCount].oUnBlurDivElement.childDivElement.style.display = "inline";
-            aIntervalLoadedImages.push(aObjects[nCount].oUnBlurDivElement.oImageElement.imageTagID);
-        }
-    }
-}, 1000);
+}
