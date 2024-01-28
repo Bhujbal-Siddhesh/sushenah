12cdffed0301f0247d6006cdba003e56ba786968
Bhujbal-Siddhesh <102919424+Bhujbal-Siddhesh@users.noreply.github.com>
2024-01-28 21:32:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,30 +8,34 @@ let aLoadedUnBlurImages = [];
     const aBlurImageLinks = [
         "https://bhujbal-siddhesh.github.io/sushenah/testImages/1.webp",
         "https://bhujbal-siddhesh.github.io/sushenah/testImages/2.webp",
-        "https://bhujbal-siddhesh.github.io/sushenah/testImages/3.webp"
+        "https://bhujbal-siddhesh.github.io/sushenah/testImages/3.webp",
+        "https://bhujbal-siddhesh.github.io/sushenah/testImages/2.webp",
+        "https://bhujbal-siddhesh.github.io/sushenah/testImages/3.webp",
+        "https://bhujbal-siddhesh.github.io/sushenah/testImages/1.webp"
     ];
     const aUnBlurImageLinks = [
         "https://bhujbal-siddhesh.github.io/sushenah/testImages/01.webp",
         "https://bhujbal-siddhesh.github.io/sushenah/testImages/02.webp",
-        "https://bhujbal-siddhesh.github.io/sushenah/testImages/03.webp"
+        "https://bhujbal-siddhesh.github.io/sushenah/testImages/03.webp",
+        "https://bhujbal-siddhesh.github.io/sushenah/testImages/02.webp",
+        "https://bhujbal-siddhesh.github.io/sushenah/testImages/03.webp",
+        "https://bhujbal-siddhesh.github.io/sushenah/testImages/01.webp"
     ];
     let aPairs = [];
     const aDivIds = ["Blur", "UnBlur"];
 
     for (let nCount = 0; nCount < aUnBlurImageLinks.length; nCount++) {
+
         let nCountTemp = nCount;
         aPairs.push("Pair_" + ++nCountTemp);
     }
-
     const oGalleryContainer = document.getElementById("galleryContainer");
 
     for (let nCount_0 = 0; nCount_0 < aPairs.length; nCount_0++) {
+
         //  creating parent div elements
         const oParentDivElement = document.createElement("div");
         oParentDivElement.setAttribute("id", aPairs[nCount_0]);
-        oParentDivElement.classList.add("d-flex");
-        // oParentDivElement.classList.add("flex-row");
-        oParentDivElement.classList.add("justify-content-center");
         oGalleryContainer.appendChild(oParentDivElement);
 
         const oParentDiv = {};
@@ -44,7 +48,22 @@ let aLoadedUnBlurImages = [];
             //  creating image elements
             const oImageElement = document.createElement("img");
             oImageElement.setAttribute("id", aPairs[nCount_0] + "_" + aDivIds[nCount_1] + "_Img");
-            oImageElement.setAttribute("alt", "");
+            oImageElement.setAttribute("alt", aPairs[nCount_0] + "_" + aDivIds[nCount_1] + "_Img");
+
+            //  bootstrap less than -lg
+            if (!(window.screen.width < 992)) {
+
+                oGalleryContainer.classList.add("container-fluid");
+                oGalleryContainer.classList.add("d-flex");
+                oGalleryContainer.classList.add("flex-wrap");
+                oGalleryContainer.classList.add("justify-content-center");
+
+                oParentDivElement.style.width = "30%";
+                oParentDivElement.classList.add("d-flex");
+                oParentDivElement.classList.add("align-items-stretch");
+
+                oImageElement.style.padding = "0.1rem";
+            }
             oImageElement.setAttribute("width", "100%");
             oImageElement.setAttribute("height", "auto");
 
@@ -52,16 +71,19 @@ let aLoadedUnBlurImages = [];
             oImageTag.imageTagID = aPairs[nCount_0] + "_" + aDivIds[nCount_1] + "_Img";
 
             if (aDivIds[nCount_1] == "Blur") {
+
                 oImageElement.setAttribute("src", aBlurImageLinks[nCount_0]);
                 oImageElement.setAttribute("onload", "loadUnBlurImages(" + oImageTag.imageTagID + ")");
                 oImageTag.imageTagElement = oImageElement;
-            } else if (aDivIds[nCount_1] == "UnBlur") {
+            }
+
+            else if (aDivIds[nCount_1] == "UnBlur") {
+
                 oImageElement.style.display = "none";
                 oImageElement.setAttribute("data-src", aUnBlurImageLinks[nCount_0]);
                 oImageElement.setAttribute("onload", "changeStyling(" + oImageTag.imageTagID + ")");
                 oImageTag.imageTagElement = oImageElement;
             }
-
             oParentDiv["o" + aDivIds[nCount_1] + "ImgElement"] = oImageTag;
             oParentDivElement.appendChild(oImageElement);
         }
@@ -71,16 +93,22 @@ let aLoadedUnBlurImages = [];
 
 function loadUnBlurImages(oImageElement) {
     aLoadedBlurImages.push(oImageElement.id);
+
     if (aLoadedBlurImages.length === aObjects.length)
+
         for (let nCount = 0; nCount < aLoadedBlurImages.length; nCount++)
+
             if (aObjects[nCount].oUnBlurImgElement.imageTagElement.hasAttribute("src") == false) {
+
                 aObjects[nCount].oUnBlurImgElement.imageTagElement.setAttribute("src", aObjects[nCount].oUnBlurImgElement.imageTagElement.getAttribute("data-src"));
             }
 }
 
 function changeStyling(oImageElement) {
     for (let nCount = 0; nCount < aObjects.length; nCount++)
+
         if (!aLoadedUnBlurImages.includes(oImageElement.id) && aObjects[nCount].oUnBlurImgElement.imageTagElement.id === oImageElement.id) {
+
             aObjects[nCount].oBlurImgElement.imageTagElement.style.display = "none";
             aObjects[nCount].oUnBlurImgElement.imageTagElement.style.display = "inline";
             aLoadedUnBlurImages.push(oImageElement.id);
